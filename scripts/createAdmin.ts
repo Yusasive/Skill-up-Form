a2383040bc69436ab11ccbadd7fb425f60ce2443
Yusuf Abdulahi <yusufabdullahi2910@gmail.com>
2025-01-18 22:24:49 +0100

    updated bsed

--- scripts/createAdmin.ts
+++ scripts/createAdmin.ts
@@ -1,20 +1,18 @@
-import bcrypt from 'bcrypt';
-import { connectToDatabase } from '@/lib/mongodb';
+// import bcrypt from 'bcrypt';
+// import { connectToDatabase } from '@/lib/mongodb';
 
-async function createAdmin() {
-  const { db } = await connectToDatabase();
+// async function createAdmin() {
+//   const { db } = await connectToDatabase();
 
-  const email = 'yusufabdullahi2910@gmail.com';
-  const password = 'Yusasive';
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+//   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const admin = await db.collection('admins').insertOne({
-    email,
-    password: hashedPassword,
-  });
+//   const admin = await db.collection('admins').insertOne({
+//     email,
+//     password: hashedPassword,
+//   });
 
-  console.log('Admin user created:', admin.insertedId);
-}
+//   console.log('Admin user created:', admin.insertedId);
+// }
 
-createAdmin().catch(console.error);
+// createAdmin().catch(console.error);
