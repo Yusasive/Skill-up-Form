20d7ba4775adf93f34959c70f575efbd1975eb45
Yusuf Abdulahi <yusufabdullahi2910@gmail.com>
2025-01-22 20:56:18 +0100

    modified  app/admin/dashboard/layout.tsx

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import Sidebar from "@/components/dashboard/Sidebar";
 
@@ -10,6 +12,24 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/admin/login");
+    } else if (session && session.user.role !== "admin") {
+      router.push("/not-authorized");
+    }
+  }, [session, status, router]);
+
+  if (status === "loading") {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="text-green-600 text-lg font-semibold">Loading ...</div>
+      </div>
+    );
+  }
 
   return (
     <SessionProvider>
