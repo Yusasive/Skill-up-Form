6b745b79d58c8ce3eb67c8668c58ee6d2271fb97
Yusuf Abdulahi <yusufabdullahi2910@gmail.com>
2025-02-04 23:52:05 +0100

    types/next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,25 +1,24 @@
-import NextAuth from "next-auth";
-
+import "next-auth";
 declare module "next-auth" {
   interface User {
-    id: string;
-    email: string;
-    role: string; 
+    id: string;  
+    email: string;  
+    role: string;  
   }
 
-  interface Session {
+  interface Session extends DefaultSession {
     user: {
-      id: string;
-      email: string;
-      role: string; 
-      name?: string | null;
-      image?: string | null;
-    };
+      id: string; 
+      email: string;  
+      role: string;  
+      name?: string | null;  
+      image?: string | null;  
+    } & DefaultSession["user"];  
   }
 
   interface JWT {
-    id: string;
-    email: string;
-    role: string;
+    id: string;  
+    email: string;  
+    role: string;  
   }
 }
