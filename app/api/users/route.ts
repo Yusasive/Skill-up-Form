ae8547ffae525b704479151cd22a3703922b68bd
Yusuf Abdulahi <yusufabdullahi2910@gmail.com>
2025-02-06 22:10:31 +0100

    app/api/users/route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getAllUsers } from "@/lib/models/user";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/authOptions";
 
 export async function GET(req: NextRequest) {
   try {
