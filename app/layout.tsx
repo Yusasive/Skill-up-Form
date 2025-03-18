86b0dfe3df5a9a986fdd9b46dbf83a51e6dc5b83
Yusuf Abdulahi <yusufabdullahi2910@gmail.com>
2025-03-18 10:37:03 +0100

    vercel web analytics tracker added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
@@ -11,7 +12,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "NAMSSN | Unilorin",
+  title: "NAMSSN || Unilorin",
   description:
     "The official website for the department of Mathematics, University of Ilorin Chapter.",
 };
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} antialiased bg-[#FFFFFF]`}>
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
