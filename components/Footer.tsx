6a9ba0356a84acf965d1a9efcea4e2b7f6ac874e
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2025-03-18 18:15:45 +0100

    Update Footer.tsx, adding underline to Yusasive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,12 @@ export default function Footer() {
   return (
     <footer className="bg-gradient-to-r from-[#252525] to-[#676767] text-white py-6 shadow-md">
       <div className="container mx-auto flex flex-col items-center text-center">
-        {/* Footer Content */}
+       
         <h2 className="text-2xl font-extrabold tracking-wide drop-shadow-lg">
           Developed by{" "}
           <a
             href="https://linkedin.com/in/yuusuf-abdullahi-temidayo-yusasive/"
-            className="text-yellow-200 hover:text-yellow-400 transition duration-300"
+            className="text-yellow-200 hover:text-yellow-400 transition duration-300 underline"
             target="_blank"
             rel="noopener noreferrer"
           >
